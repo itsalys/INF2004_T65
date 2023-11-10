2464d3134b2807f1412c01434acd2257524d6dbc
Claris Toh <claristoh0911@gmail.com>
2023-11-10 21:05:09 +0800

    push push

--- driver/main.c
+++ driver/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "lwipopts.h"
+#include "/lwip/apps/httpd.h"
 
-#include "barcode_scanner/barcode_scanner.c"
+// #include "barcode_scanner/barcode_scanner.c"
 #include "encoder/encoder.c"
 #include "magnetometer/magnetometer.c"
 #include "motor/motor.c"
 #include "ultrasonic/ultrasonic.c"
+#include "wifi/wifi.c"
 
 
 
